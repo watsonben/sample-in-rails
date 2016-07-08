9771e955844dc411ab60374d852f9f91e712b915
Ben <bw1petra@gmail.com>
2016-07-08 02:16:24 +0000

    fixed a bug with the testing. Everything works well now

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,31 +3,30 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+  @base = "Ruby on Rails Tutorial Sample App"
   end
-  
-    test "should get root" do
+
+  test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "#{@base}"
   end
-  
+
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | #{@base}"
   end
-  
+
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | #{@base}"
   end
-  
+
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact | #{@base}"
   end
-
 end
