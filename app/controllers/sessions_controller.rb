1ae7186ac4c34537664b72d94cf6e640cb1f5e7e
Ben <bw1petra@gmail.com>
2016-07-09 00:52:15 +0000

    added login functionality, and cookies

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+    
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user
+      if user.authenticate(params[:session][:password])
+        log_in user
+        redirect_to user
+      else
+        flash.now[:danger] = 'Invalid password'
+      render 'new'
+      end
+    else
+      flash.now[:danger] = 'Invalid email'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    
+  end
+end
