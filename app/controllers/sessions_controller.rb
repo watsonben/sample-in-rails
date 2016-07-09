402c52ce6e5d014c6ed62d8ac465272a52ab8f75
Ben <bw1petra@gmail.com>
2016-07-09 04:42:01 +0000

    imlemented advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user
-      if user.authenticate(params[:session][:password])
-        log_in user
-        redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user
+      if @user.authenticate(params[:session][:password])
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_to @user
       else
         flash.now[:danger] = 'Invalid password'
       render 'new'
@@ -20,7 +21,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
