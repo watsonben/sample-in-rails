d03b425742115e84d19b415518a43eac9f892d04
Ben <bw1petra@gmail.com>
2016-07-12 11:54:56 +0000

    trying to get this working withou amazon cause its silly

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,13 +1,14 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
-
+=begin
   if Rails.env.production?
     storage :fog
   else
     storage :file
   end
-
+=end
+  storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
